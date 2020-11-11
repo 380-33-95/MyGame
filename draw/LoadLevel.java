5b952e3855aa868257be8825c07149b045d1f315
globus <3803395@gmail.com>
2020-11-11 22:36:25 +0300

    07/11/2020 загрузка из файла c проверкой на размер. меню выбора уровня.
    Signed-off-by: globus <3803395@gmail.com>

--- draw/LoadLevel.java
+++ draw/LoadLevel.java
@@ -18,21 +18,23 @@ public class LoadLevel {
 
         String gh = Gdx.files.internal(NameFile).readString();
 
-        for (int ifi = 0; ifi <= gh.length() - 1; ifi++
-        ) {
-            char arr = gh.charAt(ifi);
-            AD.add(Integer.parseInt(String.valueOf(arr)));
-        }
+        if (NameFile.length() <= 140) {
 
-        int vb = 6;
-        for (int nff = 0; nff <= AD.size(); nff += 2) {
-            vb++;
-            if (AD.size() > nff) {
-                WorkBlok.LoadPoleFromBasa(vb, AD.get(nff), AD.get(nff + 1));
-                System.out.println("-> " + vb + " index " + AD.get(nff) + " storona " + AD.get(nff + 1));
+            for (int ifi = 0; ifi <= gh.length() - 1; ifi++
+            ) {
+                char arr = gh.charAt(ifi);
+                AD.add(Integer.parseInt(String.valueOf(arr)));
+            }
+
+            int vb = 6;
+            for (int nff = 0; nff <= AD.size(); nff += 2) {
+                vb++;
+                if (AD.size() > nff) {
+                    WorkBlok.LoadPoleFromBasa(vb, AD.get(nff), AD.get(nff + 1));
+                    System.out.println("-> " + vb + " index " + AD.get(nff) + " storona " + AD.get(nff + 1));
+                }
             }
         }
-
 
     }
 
