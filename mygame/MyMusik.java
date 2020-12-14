bcb19ed18e5a4ffc8259de459412b96bc473cdbd
globus <3803395@gmail.com>
2020-12-14 12:06:07 +0300

    14/12/2020 звук библиотека
    Signed-off-by: globus <3803395@gmail.com>

--- mygame/MyMusik.java
+++ mygame/MyMusik.java
@@ -1,6 +1,5 @@
 package com.mycompany.mygame;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
 import java.util.Collection;
@@ -10,45 +9,65 @@ import java.util.Set;
 public class MyMusik {
 
     ////////////// sound servic
-    public static Music start;
-    public static Music finish;
-    public static Music startBoj;
-    public static Music finishBoj;
-    public static Music smenaHoda;
+    public static Music sastavka;
+    public static Music boj;//
+    public static Music finishBoj;//
+    public static Music smenaHoda;//
+
     ///////////// sound bulet
-    public static Music buletTank;
-    public static Music buletStrelok;
-    public static Music buletRaket;
-    public static Music buletMinomet;
+//    public static Music buletTank;//
+//    public static Music buletStrelok;//
+//    public static Music buletRaket;//
+//    public static Music buletMinomet;//
+//    public static Music buletCanon;//
+
+    public static Music buletBoth;
+
     ///////////// sound start v pole
-    public static Music StartTank;
-    public static Music StartStrelok;
-    public static Music StartRaket;
-    public static Music StartMinomet;
+//    public static Music StartTank;
+//    public static Music StartStrelok;
+//    public static Music StartRaket;
+//    public static Music StartMinomet;
     public static Music StartEffect;
-    public static Music StartStena;
+//    public static Music StartStena;
+//    public static Music StartCanon;
+
+    public static Music startBoth;
+    public static Music startMine;
+
     ///////////// sound deathUnit
-    public static Music deathTank;
-    public static Music deathStrelok;
-    public static Music deathRaket;
-    public static Music deathMinomet;
+//    public static Music deathTank;//deathTank
+//    public static Music deathStrelok; //
+//    public static Music deathRaket;//
+//    public static Music deathMinomet;//
     public static Music deathEffect;
+//    public static Music deathCanon;//deathTank
+
+    public static Music deathBoth;//
+
     ///////////// sound stolknovenie
-    public static Music stolknovenieSound;
-    public static Music uronSamok;
+    public static Music stolknovenieBoth;//
+    public static Music uronZamok;//
+
+
     ///////////// sound moovUnit
-    public static Music moovTank;
-    public static Music moovStrelok;
-    public static Music moovRaket;
-    public static Music moovMinomet;
+//    public static Music moovTank;//
+//    public static Music moovStrelok;//
+//    public static Music moovRaket;//strelok
+//    public static Music moovMinomet;//strelok
+//    public static Music moovCanon;//tank
+
+    public static Music moovBoth;
+
+
+    //////////////////////////////
 
     {
-        start = Gdx.audio.newMusic(Gdx.files.internal("data/jump.wav"));
-        MapSound.put("start", start);
-        finish = Gdx.audio.newMusic(Gdx.files.internal("data/jump.wav"));
-        MapSound.put("finish", finish);
-        buletTank = Gdx.audio.newMusic(Gdx.files.internal("data/jump.wav"));
-        MapSound.put("buletTank", buletTank);
+//        startSastavka = Gdx.audio.newMusic(Gdx.files.internal("data/jump.wav"));
+//        MapSound.put("start", startSastavka);
+
+//        buletTank = Gdx.audio.newMusic(Gdx.files.internal("data/jump.wav"));
+//        MapSound.put("buletTank", buletTank);
 
     }
 
