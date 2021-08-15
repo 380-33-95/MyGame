645211eee99ee3416516d7fbbc56647ad11af26d
380-33-95 <3803395@gmail.com>
2021-08-15 22:28:48 +0300

    15.08.2021 ii 4.0

---
+++ enemy/Strategy3.java
@@ -0,0 +1,26 @@
+package com.mycompany.enemy;
+
+public class Strategy3 extends Analiz {
+
+    public static int getResult() {
+        return Result;
+    }
+
+    public static void setResult(int result) {
+        Result = result;
+    }
+
+    private static int Result;
+
+public static void Proverka(){
+
+}
+
+
+public static void Resultat(){
+
+}
+
+}//////////////////
+
+
